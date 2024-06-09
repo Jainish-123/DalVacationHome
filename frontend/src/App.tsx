09971e8f9f74366b820c75d5c8dfe3e3df6c9810
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2024-06-09 15:11:17 -0300

    feat: added 404 page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import { Home } from "./pages/Home";
+import { Agent } from "./pages/agent/Agent";
+import { P404 } from "./pages/P404/P404";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Routes>
-          <Route path='/' Component={Home}></Route>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/agent" Component={Agent}></Route>
+          <Route path="*" Component={P404}></Route>
         </Routes>
       </Router>
     </div>
