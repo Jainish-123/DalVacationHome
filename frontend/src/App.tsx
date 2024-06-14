3133bae58383a53fe840b1fc9fe13e7d2c70ce8f
Krishna Vaibhav Yadlapalli <kr732880@dal.ca>
2024-06-14 15:30:05 -0300

    Feat: added bacsic page for booking with form for booking.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Home } from "./pages/Home";
 import { Agent } from "./pages/agent/Agent";
 import { P404 } from "./pages/P404/P404";
+import { Booking } from "./pages/booking/Booking";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" Component={Home}></Route>
           <Route path="/agent" Component={Agent}></Route>
+          <Route path="/booking" Component={Booking}></Route>
           <Route path="*" Component={P404}></Route>
         </Routes>
       </Router>
