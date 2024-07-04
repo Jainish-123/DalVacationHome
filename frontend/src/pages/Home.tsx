501ba17465d39424172cc4ff152406a0a09799a0
Ketul Patel <ketulpatel3012@gmail.com>
2024-07-04 11:53:46 -0300

    fixed routing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <div className='flex flex-col gap-2'>
       <Link to={"post-query"}>Post Query</Link>
-      <Link to={"messaging"}>Messaging</Link>
+      <Link to={"messaging/123/123"}>Messaging</Link>
       <Link to={"customer-queries"}>Customer Queries</Link>
       <Link to={"agent-queries"}>Agent Queries</Link>
     </div>
